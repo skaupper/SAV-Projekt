fc44895f51e5c2f89b33f7aebb526f541eac588a
Mathias Dittrich <MathiasDittrich@gmx.at>
2020-06-06 14:24:48 +0200

    +Update: Replaced some rectangles with charts

--- CoronaTracker/CoronaTracker/ViewModels/WorldMapViewModel.cs
+++ CoronaTracker/CoronaTracker/ViewModels/WorldMapViewModel.cs
@@ -1,6 +1,8 @@
-﻿using CoronaTracker.Infrastructure;
+﻿using Chart_DevPrj;
+using CoronaTracker.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,35 @@ namespace CoronaTracker.ViewModels
 {
     class WorldMapViewModel : NotifyBase, IPageViewModel
     {
+        #region Fields
         public string Name { get { return "World Map"; } }
+        #endregion Fields
 
+        #region CTOR
+        public WorldMapViewModel()
+        {
+            HeatMap = new BindingList<HeatMapElement>{
+                new HeatMapElement
+                {
+                    Country = "US",
+                    Value = 10
+                }
+            };
+        }
+        #endregion CTOR
+
+        #region Data Bindings
+        BindingList<HeatMapElement> heatMap;
+        public BindingList<HeatMapElement> HeatMap
+        {
+            get => heatMap;
+            set
+            {
+                heatMap = value;
+                OnPropertyChanged("HeatMap");
+            }
+        }
+        #endregion Data Bindings
         #region external Methods
         public void SetupPage()
         {
