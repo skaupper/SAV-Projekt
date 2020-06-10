2e8dd56ea465037984bb9dd1502676a316a1de00
Sebastian Kaupper <kauppersebastian@gmail.com>
2020-06-10 10:57:17 +0200

    move charts to CoronaTracker project

---
+++ CoronaTracker/CoronaTracker/Charts/Types/HeatMapElement.cs
@@ -0,0 +1,8 @@
+﻿namespace CoronaTracker.Charts.Types
+{
+    public class HeatMapElement
+    {
+        public string Country { get; set; }
+        public double Value { get; set; }
+    };
+}
--- CoronaTracker/CoronaTracker/Views/WorldMapView.xaml.cs
+++ CoronaTracker/CoronaTracker/Views/WorldMapView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CoronaTracker.Charts.Types;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,38 @@ namespace CoronaTracker.Views
     /// <summary>
     /// Interaction logic for WorldmapView.xaml
     /// </summary>
-    public partial class WorldMapView : UserControl
+    public partial class WorldMapView : UserControl, INotifyPropertyChanged
     {
         public WorldMapView()
         {
+            HeatMap = new BindingList<HeatMapElement>()
+            {
+                new HeatMapElement
+                {
+                    Country = "US",
+                    Value = 123
+                }
+            };
+            DataContext = this;
             InitializeComponent();
+
+        }
+
+
+
+
+        BindingList<HeatMapElement> heatMap;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BindingList<HeatMapElement> HeatMap
+        {
+            get => heatMap;
+            set
+            {
+                heatMap = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("HeatMap"));
+            }
         }
     }
 }
